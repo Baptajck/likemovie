e39e0eca7c85bb6f022f19f766487c826e5d6641
Baptajck <parville.baptiste@gmail.com>
2019-12-12 14:46:35 +0100

    Amélioration mobile home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,25 +6,45 @@ import shows from '../../data/show';
 
 const Home = () => (
   <div className="home">
+    {/* SECTION MOVIES */}
     <div className="title">
       <h1 className="title_category">Films</h1>
     </div>
-    <div className="cover_movies">
-      <ul className="cover_movies_list">
-        {
-          movies.map((movie) => <li key={movie.id} className="cover_list"><img className="cover_img" alt="cover" title="{movie.title}" src={movie.src} /></li>)
-        }
-      </ul>
+    <div className="section">
+      <div className="cover_movies">
+        <ul className="cover_movies_list">
+          {
+            movies.map((movie) => (
+              <div className="container_movie">
+                <li key={movie.id} className="cover_list">
+                  <img className="cover_img" alt="cover" title="{movie.title}" src={movie.src} />
+                  <h2 className="title_image">{movie.title}</h2>
+                </li>
+              </div>
+            ))
+          }
+        </ul>
+      </div>
     </div>
+    {/* SECTION SHOWS */}
     <div className="title">
       <h1 className="title_category">Séries</h1>
     </div>
-    <div className="cover_shows">
-      <ul className="cover_shows_list">
-        {
-          shows.map((show) => <li key={show.id} className="cover_list"><img className="cover_img" alt="cover" title="{show.title}" src={show.src} /></li>)
-        }
-      </ul>
+    <div className="section">
+      <div className="cover_shows">
+        <ul className="cover_shows_list">
+          {
+            shows.map((show) => (
+              <div className="container_movie">
+                <li key={show.id} className="cover_list">
+                  <img className="cover_img" alt="cover" title="{show.title}" src={show.src} />
+                  <h2 className="title_image">{show.title}</h2>
+                </li>
+              </div>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   </div>
 );
